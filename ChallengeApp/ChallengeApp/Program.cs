097d762d7f40390587d0f82ec96fd8959b5c8e7c
PawelRa <pawelrat@poczta.onet.pl>
2023-01-26 19:43:07 +0100

    Zmienne

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//var myAge = 33;
+//int notMyAge = 33;
+
+//int myAge = int.MaxValue;
+//int myAge = int.MinValue;
+
+//liczby całkowite
+using System.Net.NetworkInformation;
+
+int myAge = 120;
+int newAge = myAge + 5;
+//Console.WriteLine(newAge);
+
+int myVar = -2;
+uint myVar2 = uint.MaxValue;
+uint myVar3 = uint.MinValue;
+
+long myVar4= long.MaxValue;
+long myVar5 = long.MinValue;
+
+ulong myVar6 = ulong.MaxValue;
+ulong myVar7 = ulong.MinValue;
+
+//liczby zmiennoprzecinkowe
+float myNumber = float.MaxValue;
+float Mynumber2 = float.MinValue;
+
+double myNumber3 = double.MaxValue;
+double myNumber4 = double.MinValue;
+
+decimal myNumber5 = decimal.MaxValue;
+decimal myNumber6 = decimal.MinValue;
+
+//zmienne tekstowe
+string name = "Adam";
+string surname = "nazwisko nieznane";
+string result = name + " " + surname;
+//Console.WriteLine(result);
+
+char myVar8 = 'A';
+
+var charResult = name.ToArray();
+//Console.WriteLine(charResult);
+//.WriteLine(charResult.Length);
+
+
+//zmienna logiczna
+bool isActive = true;
+isActive = false;
+
+var isVaild = 5 > 6;
+//Console.WriteLine(isVaild);
+
+var number1 = 5;
+var number2 = 10;
+
