93b02b94fb6db0b57f5c4c74998bce2c7dc3d218
PawelRa <pawelrat@poczta.onet.pl>
2023-01-26 21:20:02 +0100

    Zadanie domowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,113 +1,36 @@
-﻿//var myAge = 33;
-//int notMyAge = 33;
+﻿string name;
+bool isWomen;
+int age;
 
-//int myAge = int.MaxValue;
-//int myAge = int.MinValue;
+name = "Ola";
+age = 33;
+isWomen = true;
 
-//liczby całkowite
-using System.Net.NetworkInformation;
-
-int myAge = 120;
-int newAge = myAge + 5;
-//Console.WriteLine(newAge);
-
-int myVar = -2;
-uint myVar2 = uint.MaxValue;
-uint myVar3 = uint.MinValue;
-
-long myVar4 = long.MaxValue;
-long myVar5 = long.MinValue;
-
-ulong myVar6 = ulong.MaxValue;
-ulong myVar7 = ulong.MinValue;
-
-//liczby zmiennoprzecinkowe
-float myNumber = float.MaxValue;
-float Mynumber2 = float.MinValue;
-
-double myNumber3 = double.MaxValue;
-double myNumber4 = double.MinValue;
-
-decimal myNumber5 = decimal.MaxValue;
-decimal myNumber6 = decimal.MinValue;
-
-//zmienne tekstowe
-string name = "Adam";
-string surname = "nazwisko nieznane";
-string result = name + " " + surname;
-//Console.WriteLine(result);
-
-char myVar8 = 'A';
-
-var charResult = name.ToArray();
-//Console.WriteLine(charResult);
-//.WriteLine(charResult.Length);
-
-
-//zmienna logiczna
-bool isActive = true;
-isActive = false;
-
-var isVaild = 5 > 6;
-//Console.WriteLine(isVaild);
-
-var number1 = 5;
-var number2 = 10;
-
-//operatory relacyjne
-// ==
-// !=
-// <
-// >
-// <=
-// >=
-
-if (number1 > number2)
+if (isWomen)
 {
-    Console.WriteLine("Jestem tutaj");
-}
-else
-{
-    Console.WriteLine("Warunek nie jest spełniony");
-}
-
-//operatory logiczne
-// && - i
-// || - lub
-// ! - negacja
-
-if (name == "Adam" && myAge < 150)
-{
-    Console.WriteLine("Adam żyje");
-}
-
-if (name == "Stefan" || myAge < 150)
-{
-    Console.WriteLine("Test przeszedł");
-}
-
-
-
-if (!isActive)
-{
-    Console.WriteLine("Jest aktywny " + isActive);
-}
-else
-{
-    Console.WriteLine("Nieaktywny");
-}
-
-
-if (name == "Stefan")
-{
-    Console.WriteLine("Jesteś Stefanem");
-
-}
-else if (name == "Adam")
-{
-    Console.WriteLine("Jesteś Adamem");
-    if (myAge >= 18)
+    if (age < 30)
     {
-        Console.WriteLine("Jesteś osobą pełnoletnią");
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else
+    {
+        Console.WriteLine("Jesteś kobietą");
+    }
+}
+
+
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Jesteś mężczyzną");
     }
 }
