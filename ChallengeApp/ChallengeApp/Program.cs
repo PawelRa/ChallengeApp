1d54253ec2a20f471706d60359354444f1ce4ff9
PawelRa <pawelrat@poczta.onet.pl>
2023-01-27 19:34:26 +0100

    list

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -17,6 +17,22 @@ string[] weekdays = { "poniedziałek", "wtorek", "środa", "czwartek", "piątek"
 //pętle
 for (int i = 0; i < weekdays.Length; i++)
 {
-    Console.WriteLine(weekdays[i]);
+//    Console.WriteLine(weekdays[i]);
 }
 
+//Lista
+List<string> days = new List<string>();
+days.Add("poniedziałek");
+days.Add("wtorek2");
+days.Add("środa");
+days.Add("czwartek");
+days.Add("piątek");
+days.Add("sobota");
+days.Add("niedziela");
+
+for (int i = 0; i < days.Count; i++)
+{
+    Console.WriteLine(days[i]);
+}
+
+Console.WriteLine(days.Count);
