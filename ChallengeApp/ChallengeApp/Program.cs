9a03e8903a694c4113b37a5ceabb508023a2a0eb
PawelRa <pawelrat@poczta.onet.pl>
2023-01-28 15:50:35 +0100

    konstruktory

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,6 +1,6 @@
-﻿User user1= new User();
-User user2 = new User();
-User user3 = new User();
+﻿User user1 = new User("Adam");
+User user2 = new User("Monika","haslo");
+User user3 = new User("Zuzia","1234567890","Monika");
 User user4 = new User();
 
 
@@ -10,4 +10,27 @@ class User
     private string login;
     private string password;
     private string name;
+
+    public User()
+    {
+        this.login = "-";
+        this.password = "-";
+        this.name = "-";
+    }
+
+    public User(string login)
+    {
+        this.login = login;
+    }
+    public User(string login, string password)
+    {
+        this.login = login;
+        this.password = password;
+    }
+    public User(string login, string password, string name)
+    {
+        this.login = login;
+        this.password = password;
+        this.name = name;
+    }
 }
