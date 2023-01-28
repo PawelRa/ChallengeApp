a362b41e7769d18382a72ec5a019548c787bd615
PawelRa <pawelrat@poczta.onet.pl>
2023-01-28 15:33:52 +0100

    klasa

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿User user1= new User();
+User user2 = new User();
+User user3 = new User();
+User user4 = new User();
+
+
+
+class User
+{
+    private string login;
+    private string password;
+    private string name;
+}
