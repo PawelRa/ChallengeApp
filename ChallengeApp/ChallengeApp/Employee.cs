7fa003c38dd38790ff1a7b588621ef5255a26e4d
PawelRa <pawelrat@poczta.onet.pl>
2023-01-29 23:10:36 +0100

    Zadanie domowe dzień 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private int score = 0;
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+        public int result { get { return score; } }
+        public void addScore(int score)
+        {
+            this.score += score;
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -7,12 +7,55 @@ User user3 = new User("Zuzia", "1234567890");
 User user4 = new User("Damian", "32423fddf");
 
 //user1.Login = "Marek";
-Console.WriteLine(user2.Login);
+//Console.WriteLine(user2.Login);
 user1.AddScore(5);
 user1.AddScore(2);
 var result = user1.Result;
-Console.WriteLine(result);
+//Console.WriteLine(result);
 
-Console.WriteLine(User.GameName);
+//Console.WriteLine(User.GameName);
 var pi = Math.PI;
 
+Employee employee1 = new Employee("Jan", "Nowak", 25);
+Employee employee2 = new Employee("Zenon", "Kowalski", 38);
+Employee employee3 = new Employee("Maria", "Wiśniewska", 24);
+
+employee1.addScore(1);
+employee1.addScore(2);
+employee1.addScore(3);
+employee1.addScore(4);
+employee1.addScore(5);
+employee2.addScore(2);
+employee2.addScore(4);
+employee2.addScore(6);
+employee2.addScore(8);
+employee2.addScore(10);
+employee3.addScore(7);
+employee3.addScore(7);
+employee3.addScore(7);
+employee3.addScore(7);
+employee3.addScore(7);
+
+int[] scores = new int[] { employee1.result, employee2.result, employee3.result };
+int max = 0;
+
+for (int i = 1; i < scores.Length; i++)
+{
+    if (scores[i] > scores[i - 1])
+    {
+        max = i;
+    }
+}
+
+if (max == 0)
+{
+    Console.WriteLine(employee1.Name + " " + employee1.Surname + " is a winner and has " + employee1.result + " points");
+}
+else if (max == 1)
+{
+    Console.WriteLine(employee2.Name + " " + employee2.Surname + " is a winner and has " + employee2.result + " points");
+}
+else
+{
+    Console.WriteLine(employee3.Name + " " + employee3.Surname + " is a winner and has " + employee3.result + " points");
+}
