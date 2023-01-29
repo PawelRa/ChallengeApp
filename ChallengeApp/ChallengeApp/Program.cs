663d5639fd5412f7b3810be0a31eae951f73aba9
PawelRa <pawelrat@poczta.onet.pl>
2023-01-29 15:33:46 +0100

    Pola w klasach

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,36 +1,39 @@
-﻿User user1 = new User("Adam");
-User user2 = new User("Monika","haslo");
-User user3 = new User("Zuzia","1234567890","Monika");
-User user4 = new User();
+﻿User user1 = new User("Adam", "sgksdkjg354");
+User user2 = new User("Monika", "haslo");
+User user3 = new User("Zuzia", "1234567890", "Monika");
+User user4 = new User("Damian", "32423fddf");
 
-
+//user1.Login = "Marek";
+Console.WriteLine(user2.Login);
 
 class User
 {
-    private string login;
-    private string password;
     private string name;
 
     public User()
     {
-        this.login = "-";
-        this.password = "-";
-        this.name = "-";
+        this.Login = "-";
+        this.Password = "-";
+        this.Name = "-";
     }
 
     public User(string login)
     {
-        this.login = login;
+        this.Login = login;
     }
     public User(string login, string password)
     {
-        this.login = login;
-        this.password = password;
+        this.Login = login;
+        this.Password = password;
     }
     public User(string login, string password, string name)
     {
-        this.login = login;
-        this.password = password;
-        this.name = name;
+        this.Login = login;
+        this.Password = password;
+        this.Name = name;
     }
+
+    public string Login { get;private set; }
+    public string Password { get; private set; }
+    public string Name { get; private set; }
 }
