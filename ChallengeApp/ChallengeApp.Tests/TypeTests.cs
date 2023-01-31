6ece1f26d813fd6945639380b6e9ebcda8d5863a
PawelRa <pawelrat@poczta.onet.pl>
2023-01-31 17:15:14 +0100

    Metody prywatne

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,32 @@
+﻿
+
+using NUnit.Framework;
+
+
+namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+
+        [Test]
+        public void TEST()
+        {
+            //arrange
+
+            var user1 = GetUser("Adam");
+            var user2 = GetUser("Tomasz");
+
+            //act
+
+
+            //assert
+
+        }
+        private User GetUser(string name)
+        {
+            return new User(name, "haslo");
+        }
+
+
+    }
+}
