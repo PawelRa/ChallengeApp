2933a9c4de7474801388a4adb3fb321345d8f32c
PawelRa <pawelrat@poczta.onet.pl>
2023-01-31 19:17:29 +0100

    Zadanie domowe - dzień 8

--- ChallengeApp/ChallengeApp.Tests/TypeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -1,54 +1,78 @@
-﻿
-
-using NUnit.Framework;
-
-
+﻿using NUnit.Framework;
 namespace ChallengeApp.Tests
+
 {
     public class TypeTests
     {
-
         [Test]
-        public void TestObiektu()
+        public void IntValueVariableTest()
         {
             //arrange
-
-            var user1 = GetUser("Adam");
-            var user2 = GetUser("Adam");
+            int number1 = 2;
+            int number2 = 2;
 
             //act
-
+            int result = number1 + number2;
 
             //assert
-            Assert.AreEqual(user1, user2);
-
-
+            Assert.AreEqual(4, result);
+            Assert.AreEqual((int)number1, number2);
         }
 
         [Test]
-        public void TestWartosci()
+        public void DoubleValueVariableTest()
         {
             //arrange
+            double number1 = 1.654879658465;
+            double number2 = 1.654879658465;
 
+            //act
+            double result = number1 + number2 + 1;
+
+            //assert
+            Assert.AreEqual(number1, number2);
+            Assert.AreEqual(number1 + number2 + 1, result);
+        }
+
+
+        [Test]
+        public void BoolValueVariableTest()
+        {
+            //arrange
+            bool number1 = true;
+            bool number2 = false;
+
+            //assert
+            Assert.AreNotEqual(number1, number2);
+        }
+
+        [Test]
+        public void StringTest()
+        {
+            //arrange
+            string text1 = "Ola ma kota";
+            string text2 = "Ola ma kota";
+
+            //assert
+            Assert.AreEqual(text1, text2);
+        }
+
+
+        [Test]
+        public void ReferenceVariableTests()
+        {
+            //arrange
             var user1 = GetUser("Adam");
             var user2 = GetUser("Adam");
 
-            //act
-
-
             //assert
-
-            Assert.AreEqual(user1.Login, user2.Login);
-
-
+            Assert.AreNotEqual(user1, user2);
+            Assert.AreEqual((user1.Login, user1.Password), (user2.Login, user2.Password));
         }
-
 
         private User GetUser(string name)
         {
             return new User(name, "haslo");
         }
-
-
     }
 }
