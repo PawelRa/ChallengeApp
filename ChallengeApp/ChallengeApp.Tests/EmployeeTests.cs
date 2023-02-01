3a9c334c59e27138c64240efa6b8987ec5a874c3
PawelRa <pawelrat@poczta.onet.pl>
2023-02-01 20:26:39 +0100

    Test Code Refactor

--- ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -1,71 +1,52 @@
 ﻿using NUnit.Framework;
-using System.Runtime.Intrinsics.X86;
 
 namespace ChallengeApp.Tests
 {
     public class EmployeeTests
     {
         [Test]
-        public void MinTest()
+        public void EmployeesMinMaxAverageTest()
         {
             //arrange
-            Employee employee1 = new Employee("Jan", "Nowak");
+            Employee employee = new Employee("Jan", "Nowak");
 
             //act
-            employee1.AddGrade(2);
-            employee1.AddGrade(5);
-            employee1.AddGrade(7);
-            employee1.AddGrade(1);
-            var minResult = employee1.GetStatistics();
+            employee.AddGrade(2);
+            employee.AddGrade(5);
+            employee.AddGrade(7);
+            employee.AddGrade(1);
+            var result = employee.GetStatistics();
 
             //assert            
-            Assert.AreEqual(1, minResult.Min);
+            Assert.AreEqual(1, result.Min);
+            Assert.AreEqual(7, result.Max);
+            Assert.AreEqual(3.75, result.Average);
         }
 
         [Test]
-        public void MaxTest()
+        public void EmployeesNoGradeTest()
         {
             //arrange
-            Employee employee2 = new Employee("Zenon", "Kowalski");
+            Employee employee1 = new Employee("Zenon", "Kowalski");
 
             //act
-            employee2.AddGrade(2);
-            employee2.AddGrade(5);
-            employee2.AddGrade(7);
-            employee2.AddGrade(1);
-            var maxResult = employee2.GetStatistics();
+            var noGradeResult = employee1.GetStatistics();
 
             //assert            
-            Assert.AreEqual(7, maxResult.Max);
+            Assert.AreEqual(noGradeResult.Average,float.NaN);
         }
 
-        [Test]
-        public void AverageTest()
-        {
-            //arrange
-            Employee employee3 = new Employee("Zenon", "Kowalski");
-
-            //act
-            employee3.AddGrade(2);
-            employee3.AddGrade(5);
-            employee3.AddGrade(7);
-            employee3.AddGrade(1);
-            var averageResult = employee3.GetStatistics();
-
-            //assert            
-            Assert.AreEqual(3.75, averageResult.Average);
-        }
 
         [Test]
         public void EmployeesComparisonTest()
         {
             //arrange
-            Employee employee4 = new Employee("Zenon", "Kowalski");
-            Employee employee5 = new Employee("Zenon", "Kowalski");
+            Employee employee2 = new Employee("Zenon", "Kowalski");
+            Employee employee3 = new Employee("Zenon", "Kowalski");
 
             //assert            
-            Assert.AreNotEqual(employee4 ,employee5);
-            Assert.AreEqual((employee4.Name, employee4.Surname), (employee5.Name, employee5.Surname));
+            Assert.AreNotEqual(employee2 ,employee3);
+            Assert.AreEqual((employee2.Name, employee2.Surname), (employee3.Name, employee3.Surname));
         }
     }
 }
