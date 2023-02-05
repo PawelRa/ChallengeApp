cd695b7dbcc275ad7176b10d9cd0086717e8cd07
PawelRa <pawelrat@poczta.onet.pl>
2023-02-05 18:13:38 +0100

    pętla while

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -90,13 +90,22 @@
             //}
 
             var index = 0;
-            do //pętla do - while zawsze wykona się przynajmniej 1x
+            //do //pętla do - while zawsze wykona się przynajmniej 1x
+            //{
+            //    statistics.Max = Math.Max(statistics.Max, this.grades[index]);
+            //    statistics.Min = Math.Min(statistics.Min, this.grades[index]);
+            //    statistics.Average += this.grades[index];
+            //    index++;
+            //} while (index < this.grades.Count);
+
+            while (index < this.grades.Count) //pętla while
             {
                 statistics.Max = Math.Max(statistics.Max, this.grades[index]);
                 statistics.Min = Math.Min(statistics.Min, this.grades[index]);
                 statistics.Average += this.grades[index];
                 index++;
-            } while (index < this.grades.Count);
+            } 
+
 
             //statistics.Average = statistics.Average / this.grades.Count;
             statistics.Average /= this.grades.Count;
